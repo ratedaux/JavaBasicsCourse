d509d3bd9138ce3cf7c16aed3e02dca9a1a8d1be
KKibirieva <kkibirieva@gmail.com>
2024-10-01 17:32:58 +0200

    homework16

--- src/homework16/MagicArray2.java
+++ src/homework16/MagicArray2.java
@@ -11,16 +11,16 @@ public class MagicArray2 {
 
     void add(int value) {
         if (cursor == array.length - 1) {
-        expandArray();
+            expandArray();
         }
         array[cursor] = value;
         cursor++;
     }
 
-    void add(int... numbers){
+    void add(int... numbers) {
         //System.out.println("Accepted a few ints. To be precise " + numbers.length);
         //System.out.println("Under index 0 is " + numbers[0]);
-        for(int i=0; i<numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             add(numbers[i]);
         }
     }
@@ -29,41 +29,42 @@ public class MagicArray2 {
         System.out.println("Expanding array... Cursor = " + cursor);
 
         int[] newArray = new int[array.length * 2];
-        for (int i=0; i<cursor; i++) {
-            newArray[i]=array[i];
+        for (int i = 0; i < cursor; i++) {
+            newArray[i] = array[i];
         }
 
-        array=newArray;
+        array = newArray;
         System.out.println("Array expanded.");
     }
 
-    public String toString(){
-        if(cursor==0){
+    public String toString() {
+        if (cursor == 0) {
             return "[]";
-        }        String result ="[";
-        for(int i=0; i< cursor; i++){
-            result=result+array[i]+(i<cursor-1 ? ", " : "]");
+        }
+        String result = "[";
+        for (int i = 0; i < cursor; i++) {
+            result = result + array[i] + (i < cursor - 1 ? ", " : "]");
         }
         return result;
     }
 
-    int size(){
+    int size() {
         return cursor;
     }
 
     //возвращает значение по индексу
-    int get(int index){
-        if (index >=0 && index<cursor) {
+    int get(int index) {
+        if (index >= 0 && index < cursor) {
             return array[index];
         }
         return Integer.MIN_VALUE; // no good solution
     }
 
-    int remove(int index){
-        int deleted=array[index];
-        if(index>=0){
-            for (int i=index; i<array.length; i++) {
-                array[i-1] = array[i];
+    int remove(int index) {
+        int deleted = array[index];
+        if (index >= 0) {
+            for (int i = index; i < array.length; i++) {
+                array[i - 1] = array[i];
             }
             cursor--;
         }
--- src/homework16/MainArray.java
+++ src/homework16/MainArray.java
@@ -6,7 +6,7 @@ import lesson16.MagicArray;
 public class MainArray {
 
     public static void main(String[] args) {
-        MagicArray2 magicArray=new MagicArray2();
+        MagicArray2 magicArray = new MagicArray2();
 
         magicArray.add(100);
         magicArray.add(200);
@@ -16,14 +16,13 @@ public class MainArray {
 
         System.out.println("There are " + magicArray.size() + " elements in array up to now.");
 
-        magicArray.add(500,600,700,800,900,1000,1100,1200,1300,1400,1500);
+        magicArray.add(500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500);
         System.out.println(magicArray.toString());
 
         magicArray.remove(5);
         System.out.println("Result array: " + magicArray.toString());
 
         System.out.println(magicArray.get(5));
-
 
 
     }
